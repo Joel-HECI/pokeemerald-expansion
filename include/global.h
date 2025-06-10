54c1ea0d180676632c55ceb2e746d5dc3e4a1202
joel <joel.hernandez-c@mail.escuelaing.edu.co>
2025-06-09 21:05:33 -0500

    character property

--- include/global.h
+++ include/global.h
@@ -564,8 +564,8 @@ struct RankingHall2P
 struct SaveBlock2
 {
     /*0x00*/ u8 playerName[PLAYER_NAME_LENGTH + 1];
-    /*0x08*/ u8 playerGender;    // MALE, FEMALE
-    /*0x09*/ u8 playerCharacter; // PLAYER_CHARACTER_[RED/BRANDON/EMERALD]
+    /*0x08*/ u8 playerGender; // MALE, FEMALE
+    u8 playerCharacter;       // PLAYER_CHARACTER_[RED/BRANDON/EMERALD]
     /*0x09*/ u8 specialSaveWarpFlags;
     /*0x0A*/ u8 playerTrainerId[TRAINER_ID_LENGTH];
     /*0x0E*/ u16 playTimeHours;
@@ -579,8 +579,8 @@ struct SaveBlock2
     u16 optionsBattleStyle : 1;        // OPTIONS_BATTLE_STYLE_[SHIFT/SET]
     u16 optionsBattleSceneOff : 1;     // whether battle animations are disabled
     u16 regionMapZoom : 1;             // whether the map is zoomed in
-    // u16 padding1:4;
-    // u16 padding2;
+                                       // u16 padding1:4;
+                                       // u16 padding2;
     /*0x18*/ struct Pokedex pokedex;
     /*0x90*/ u8 filler_90[0x8];
     /*0x98*/ struct Time localTimeOffset;
